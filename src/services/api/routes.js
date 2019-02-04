712355cb029ce4685460ecc4317f324fe3b53299
macdja38 <jakeincanada@icloud.com>
2019-02-04 03:59:42 -0500

    Remove named exports from routes

--- src/services/api/routes.js
+++ src/services/api/routes.js
@@ -1,9 +1,9 @@
 const API_ROOT = 'http://localhost:8000/api/';
 
-export const games = `${API_ROOT}games/`;
-export const game = id => `${API_ROOT}games/${id}/`;
-export const flag = id => `${game(id)}flag/`;
-export const reveal = id => `${game(id)}reveal/`;
+const games = `${API_ROOT}games/`;
+const game = id => `${API_ROOT}games/${id}/`;
+const flag = id => `${game(id)}flag/`;
+const reveal = id => `${game(id)}reveal/`;
 
 export default {
   games,
